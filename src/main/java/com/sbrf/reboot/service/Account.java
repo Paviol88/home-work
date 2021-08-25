f211881eca551c3b6f81145264e49ec542017f00
paviol88 <sergunin.pavel@gmail.com>
2021-08-26 00:02:51 +0300

    Домашка урок 3: создание иерархии классов- изменил аннотацию lombok

--- src/main/java/com/sbrf/reboot/service/Account.java
+++ src/main/java/com/sbrf/reboot/service/Account.java
@@ -1,9 +1,12 @@
 package com.sbrf.reboot.service;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Account {
     private String accnum;
 
-    public Account(String accnum) {
+  /* public Account(String accnum) {
         this.accnum = accnum;
-    }
+    }*/
 }
--- src/main/java/com/sbrf/reboot/service/AccountService.java
+++ src/main/java/com/sbrf/reboot/service/AccountService.java
@@ -1,11 +1,14 @@
 package com.sbrf.reboot.service;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class AccountService {
     private AccountRepository accountRepository;
 
-    public AccountService(AccountRepository accountRepository) {
+    /*public AccountService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
-    }
+    }*/
 
     public boolean isAccountExist(long id, Account account) {
         return accountRepository.getAllAccountsByClientId(id).contains(account);
