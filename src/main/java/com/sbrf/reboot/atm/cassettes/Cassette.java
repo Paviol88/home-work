b59db141b4cc35041695c4c06ed3836f009be949
paviol88 <sergunin.pavel@gmail.com>
2021-08-30 15:55:31 +0300

    Решение конфликта

---
+++ src/main/java/com/sbrf/reboot/atm/cassettes/Cassette.java
@@ -0,0 +1,11 @@
+package com.sbrf.reboot.atm.cassettes;
+
+import java.util.ArrayList;
+
+public class Cassette {
+    public Cassette(ArrayList<com.sbrf.reboot.atm.cassettes.CassetteTest.OneHundred> oneHundreds) {
+    }
+
+    public int getCountBanknotes() {
+    }
+}
